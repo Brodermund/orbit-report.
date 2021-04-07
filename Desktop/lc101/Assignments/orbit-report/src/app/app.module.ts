7d66c9dde975d897c752f4be81093252cc9ed184
Brodermund <rodermundbobby@yahoo.com>
2021-04-07 05:44:10 -0500

    stage 8 complete

--- Desktop/lc101/Assignments/orbit-report/src/app/app.module.ts
+++ Desktop/lc101/Assignments/orbit-report/src/app/app.module.ts
@@ -3,15 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { OrbitListComponent } from './orbit-list/orbit-list.component';
-import { HighlightDirective } from './highlight.directive';
-import { WarningDirective } from './warning.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrbitListComponent,
-    HighlightDirective,
-    WarningDirective
+    
   ],
   imports: [
     BrowserModule
